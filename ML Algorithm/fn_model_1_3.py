e50820323ad15da325882728f5641d16b9b505cc
JHann2701 <128495673+JHann2701@users.noreply.github.com>
2023-04-14 00:17:47 -0400

    Update fn_model_1_3.py

--- ML Algorithm/fn_model_1_3.py
+++ ML Algorithm/fn_model_1_3.py
@@ -1,8 +1,11 @@
 from torch import nn as nn
+# This line imports the nn module from the PyTorch library. This module provides a wide range of neural network layers and functionalities
 from fn_try_gpu import *
+# This line imports a function called try_gpu() from the fn_try_gpu module to check if a GPU is available and return the appropriate device
 
 
 def init_model():
+# This line defines a function called init_model() which will initialize and return a PyTorch Sequential model object
     return nn.Sequential(nn.Linear(42, 32),
                          nn.ReLU(),
                          nn.Linear(32, 32),
@@ -10,3 +13,5 @@ def init_model():
                          nn.Linear(32, 16),
                          nn.ReLU(),
                          nn.Linear(16, 12),).to(device=try_gpu())
+    # Overall, this code defines a PyTorch neural network with four fully connected layers and ReLU activation functions in between 
+    # The init_model() function can be called to instantiate the model and set the device to be used for computation
